1f2cfe243c9ba5d05a1125dd01d5bc3c1f712aff
João Marcos <101360915+joaomarcosmr@users.noreply.github.com>
2025-10-24 08:20:52 -0300

    first commit

---
+++ src/test/java/com/challenge/geosapiens/service_c/ServiceCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.geosapiens.service_c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
